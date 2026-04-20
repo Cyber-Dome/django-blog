2be0ccb065ecee2267231689513480aa1c003e1d
Cyber-Dome <ajiamal2002@gmail.com>
2026-04-20 13:20:58 +0530

    blogs and search features

--- blogs/models.py
+++ blogs/models.py
@@ -34,3 +34,24 @@ class Blog(models.Model):
 
     def __str__(self):
         return self.title
+
+class About(models.Model):
+    about_heading = models.CharField(max_length=100)
+    about_description = models.TextField(max_length=225)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        verbose_name_plural = "About"
+
+    def __str__(self):
+        return self.about_heading
+
+class SocialLinks(models.Model):
+    platform = models.CharField(max_length=50)
+    link = models.URLField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.platform
